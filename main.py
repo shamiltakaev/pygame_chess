a17697f4adc0e2b49856e5eca0c10804d32ad1eb
SHAMILTAKAEV <shamilchechya@gmail.com>
2023-11-27 17:32:23 +0300

    Add urls

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 import pygame
 pygame.init()
+# https://thepythoncode.com/article/make-a-chess-game-using-pygame-in-python
+# https://github.com/x4nth055/pythoncode-tutorials/blob/master/gui-programming/chess-game/data/classes/Board.py
